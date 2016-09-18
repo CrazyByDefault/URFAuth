661168f21a95f85dd4b7cd2a71ab97da617b1cf6
Manish <manish97ravula@gmail.com>
2016-09-18 07:56:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import serial
+import sys
+
+route = '/dev/ttyUSB2'
+s = serial.Serial(route,9600)
+
+
+def read_key():
+    i=0
+    while(i<1000):
+        print(1)
+        k = s.readline()
+       # if k.splitlines() == [' 47 C8 12 2B ']:
+        #    return 1
+        print(k)
+        if(k):
+            return k
+           #break;
+
+def ask_key():
+    s.write('key')
+
+def authenticate():
+    ask_key()
+    key_recv = read_key().splitlines() 
+    print key_recv
+    if key_recv == [' 47 C8 12 2B ']:
+        sys.exit(0)
+    #code = read_key()
+    #if code!=1:
+    #    sys.exit(0)
+    else:
+        print ("Authentication failed. Received ")
+        sys.exit(-1)
+
+
+authenticate()
+sys.exit(-1)
